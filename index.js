56c20c6c77ec03a4b7bb1835f153393871f0c38a
quangphamit2018 <92535010+quangphamit2018@users.noreply.github.com>
2026-04-18 22:21:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const app = express();
 
 app.use(express.json());
 
+// 👉 route test
 app.get("/", (req, res) => {
   res.send("Nexa bot running 🚀");
 });
 
+// ✅ THÊM ĐOẠN NÀY (verify Zalo)
+app.get("/zalo_verifier_JC6xT9RgAc9h_wLFb-u6QKVvecAhZtS_Cpau.html", (req, res) => {
+  res.send("zalo-site-verification: zalo_verifier_JC6xT9RgAc9h_wLFb-u6QKVvecAhZtS_Cpau.html");
+});
+
+// 👉 webhook Zalo
 app.post("/webhook", (req, res) => {
   console.log("Webhook nhận:", req.body);
   res.status(200).send("OK");
